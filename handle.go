959ba171d898ebe4f4a9de34a4c69260e3c381e2
Shashankft9 <shanky.337marchss@gmail.com>
2022-04-01 03:29:41 -0400

    cleanup

--- handle.go
+++ handle.go
@@ -8,6 +8,6 @@ import (
 
 // Handle an HTTP Request.
 func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) {
-        fmt.Println("received a request")       // Print "OK" to standard output (local logs)
-        fmt.Fprintln(res, "Hello from KCD Chennai! Read more about us here: https://community.cncf.io/events/details/cncf-kcd-chennai-presents-kubernetes-community-days-chennai-2022/") // Send "OK" back to the client
+        fmt.Println("received a request")
+        fmt.Fprintln(res, "Hello from KCD Chennai! Read more about us here: https://community.cncf.io/events/details/cncf-kcd-chennai-presents-kubernetes-community-days-chennai-2022/") // Send KCD community link back to the client
 }
